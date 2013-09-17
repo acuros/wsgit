b3d2b9f4ea9addeff232a622d51668b5a2bff08e
Acuros <acuros@cnpdev.cafe24.com>
2013-09-17 19:55:24 +0900

    * Reset for new project

--- management/commands/runimdjango.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from imdjango.network.server import IMServer
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        IMServer.start_server(*args)
--- utils.py
+++
@@ -1,6 +0,0 @@
-from django.core import serializers
-
-def serialize_page(page, fields=[]):
-    serialized_page = dict(num_pages=page.paginator.num_pages, number=page.number)
-    serialized_page['object_list'] = serializers.serialize("python", page.object_list)
-    return serialized_page
