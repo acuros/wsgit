46b3ac3ea4b7b4d0113e0e541b8b93a87f2f888b
Acuros <acuros@MacBookAir>
2013-09-18 19:03:13 +0900

    remote_addr, remote_port test code added - #4

--- tests/test_environ.py
+++ tests/test_environ.py
@@ -2,16 +2,17 @@
 '''Tests for Environ object'''
 
 import unittest
-from wsgit import Environ
+from wsgit import Environ, Request
 
-def environ(request):
-    return Environ(request).get_dict()
+def environ(request_parameters, meta={}):
+    return Environ({'parameters':request_parameters, 'meta':meta).get_dict()
 
 class TestEnviron(unittest.TestCase):
     def test_request_method(self):
         self.assertEqual(environ({})['REQUEST_METHOD'], 'MOBILE')
 
     def test_reqeust_uri(self):
+        request = 
         self.assertEqual(environ({})['REQUEST_URI'], None)
         self.assertEqual(environ({'uri':'/'})['REQUEST_URI'], '/')
         self.assertEqual(environ({'uri':'/foo/bar/'})['REQUEST_URI'],
@@ -22,13 +23,23 @@ class TestEnviron(unittest.TestCase):
     def test_path_info(self):
         self.assertEqual(environ({}).get('PATH_INFO'), None)
         self.assertEqual(environ({'url':'/'})['PATH_INFO'], '/')
-        self.assertEqual(environ({'url': '/foo/bar/?foo=bar'}['PATH_INFO'], 
-                                 '/foo/bar/'))
+        self.assertEqual(environ({'url': '/foo/bar/?foo=bar'})['PATH_INFO'], 
+                                 '/foo/bar/')
+        self.assertEqual(environ({'url':'/foo//'})['PATH_INFO'], '/foo/')
 
     def test_query_string(self):
         self.assertEqual(environ({}).get('QUERY_STRING'), None)
         self.assertEqual(environ({'url':'/'}).get('QUERY_STRING'), '')
-        self.assertEqual(environ({'url': '/foo/bar/?foo=bar'}['PATH_INFO'], 
-                                 'foo=bar'))
-        self.assertEqual(environ({'url': '/foo/bar/?foo=bar&foo2=bar2'}['PATH_INFO'], 
-                                 'foo=bar&foo2=bar2'))
+        self.assertEqual(environ({'url': '/foo/bar/?foo=bar'})['PATH_INFO'], 
+                                 'foo=bar')
+        self.assertEqual(environ({'url': '/?foo=bar&foo2=bar2'})['PATH_INFO'], 
+                                 'foo=bar&foo2=bar2')
+
+    def test_remote_addr(self):
+        self.assertEqual(Environ({}).get_dict().get('REMOTE_ADDR'), None)
+        self.assertEqual(environ(meta={'ip':'127.0.0.1'})['REMOTE_ADDR'],
+                                 '127.0.0.1')
+
+    def test_remote_port(self):
+        self.assertEqual(Environ({}).get_dict().get('REMOTE_PORT'), None)
+        self.assertEqual(environ(meta={'port':10295})['REMOTE_PORT'], '10295')
