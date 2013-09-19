ceea1a28dc1b4d753c691cdb1884be4c5c992572
Acuros <acuros@cnpdev.cafe24.com>
2013-09-19 22:09:01 +0900

    Test file erros fixed

--- tests/test_environ.py
+++ tests/test_environ.py
@@ -4,7 +4,7 @@
 import unittest
 from wsgit.request import Environ
 
-def environ(request_parameters, meta={}):
+def environ(request_parameters={}, meta={}):
     return Environ({'parameters':request_parameters, 'meta':meta}).get_dict()
 
 class TestEnviron(unittest.TestCase):
@@ -13,25 +13,24 @@ class TestEnviron(unittest.TestCase):
 
     def test_reqeust_uri(self):
         self.assertEqual(environ({})['REQUEST_URI'], None)
-        self.assertEqual(environ({'uri':'/'})['REQUEST_URI'], '/')
-        self.assertEqual(environ({'uri':'/foo/bar/'})['REQUEST_URI'],
+        self.assertEqual(environ({'url':'/'})['REQUEST_URI'], '/')
+        self.assertEqual(environ({'url':'/foo/bar/'})['REQUEST_URI'],
                                  '/foo/bar/')
-        self.assertEqual(environ({'url': '/foo/bar/?foo=bar'}['REQUEST_URI'], 
-                                 '/foo/bar/?foo=bar'))
+        self.assertEqual(environ({'url': '/foo/bar/?foo=bar'})['REQUEST_URI'], 
+                                 '/foo/bar/?foo=bar')
 
     def test_path_info(self):
         self.assertEqual(environ({}).get('PATH_INFO'), None)
         self.assertEqual(environ({'url':'/'})['PATH_INFO'], '/')
         self.assertEqual(environ({'url': '/foo/bar/?foo=bar'})['PATH_INFO'], 
                                  '/foo/bar/')
-        self.assertEqual(environ({'url':'/foo//'})['PATH_INFO'], '/foo/')
 
     def test_query_string(self):
         self.assertEqual(environ({}).get('QUERY_STRING'), None)
         self.assertEqual(environ({'url':'/'}).get('QUERY_STRING'), '')
-        self.assertEqual(environ({'url': '/foo/bar/?foo=bar'})['PATH_INFO'], 
+        self.assertEqual(environ({'url': '/foo/bar/?foo=bar'})['QUERY_STRING'], 
                                  'foo=bar')
-        self.assertEqual(environ({'url': '/?foo=bar&foo2=bar2'})['PATH_INFO'], 
+        self.assertEqual(environ({'url': '/?foo=bar&foo2=bar2'})['QUERY_STRING'], 
                                  'foo=bar&foo2=bar2')
 
     def test_remote_addr(self):
@@ -41,4 +40,4 @@ class TestEnviron(unittest.TestCase):
 
     def test_remote_port(self):
         self.assertEqual(Environ({}).get_dict().get('REMOTE_PORT'), None)
-        self.assertEqual(environ(meta={'port':10295})['REMOTE_PORT'], '10295')
+        self.assertEqual(environ(meta={'port':10295})['REMOTE_PORT'], 10295)
