c2845cccbd035ce11c88f6f0a90d6723a791ea13
acuros <acuros@kaist.ac.kr>
2013-10-04 02:21:41 +0900

    Useless line removed

--- tests/applications.py
+++ tests/applications.py
@@ -1,6 +1,3 @@
-from djangoproject.djangoproject.wsgi import application as django_application
-
-
 def various_status_application(environ, start_response):
     import json
     status = environ.get('QUERY_STRING') or '200 OK'
