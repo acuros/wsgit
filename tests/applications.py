0954302496672f9880cc4e9577f0be346ad4cb30
acuros <acuros@kaist.ac.kr>
2014-09-13 22:35:40 +0900

    useless import

--- tests/applications.py
+++ tests/applications.py
@@ -1,5 +1,4 @@
 def various_status_application(environ, start_response):
-    import json
     status = environ.get('QUERY_STRING') or '200 OK'
     start_response(status, [('Content-Type', 'application/json')])
 
--- tests/test_server.py
+++ tests/test_server.py
@@ -2,7 +2,6 @@ import bson
 import os
 import ssl
 import unittest
-from threading import Thread
 from wsgit.server import Server
 from tests.applications import various_status_application as app
 from socket import socket, SOCK_STREAM, AF_INET
