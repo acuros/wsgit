daaca6f8bfdc508dbed3cd7e3574a1a2a651a128
acuros <acuros@kaist.ac.kr>
2014-09-13 23:14:42 +0900

    Code style

--- tests/test_wsgi_handler.py
+++ tests/test_wsgi_handler.py
@@ -8,30 +8,49 @@ from tests.applications import various_status_application,\
 class TestWSGIHandler(unittest.TestCase):
 
     def test_call_application(self):
-        request = dict(meta=dict(ip='127.0.0.1', port=19234),
-                       parameters=dict(url='/'))
+        request = dict(
+            meta=dict(ip='127.0.0.1', port=19234),
+            parameters=dict(url='/')
+        )
         environ, handler = Environ(request), WSGIHandler()
-        bson_binary = handler.call_application(various_status_application,
-                                               environ.get_dict())
-        self.assertEqual(dict(status=dict(reason='OK', code='200')),
-                         bson.loads(bson_binary))
+        bson_binary = handler.call_application(
+            various_status_application,
+            environ.get_dict()
+        )
+        self.assertEqual(
+            dict(status=dict(reason='OK', code='200')),
+            bson.loads(bson_binary)
+        )
 
     def test_not_found_status(self):
-        request = dict(meta=dict(ip='127.0.0.1', port=19234),
-                       parameters=dict(url='/?404 NOT FOUND'))
+        request = dict(
+            meta=dict(ip='127.0.0.1', port=19234),
+            parameters=dict(url='/?404 NOT FOUND')
+        )
         environ, handler = Environ(request), WSGIHandler()
-        bson_binary = handler.call_application(various_status_application,
-                                               environ.get_dict())
-        self.assertEqual(dict(status=dict(reason='NOT FOUND', code='404')),
-                         bson.loads(bson_binary))
+        bson_binary = handler.call_application(
+            various_status_application,
+            environ.get_dict()
+        )
+        self.assertEqual(
+            dict(status=dict(reason='NOT FOUND', code='404')),
+            bson.loads(bson_binary)
+        )
 
     def test_no_json_response(self):
-        request = dict(meta=dict(ip='127.0.0.1', port=19234),
-                       parameters=dict(url='/?404 NOT FOUND'))
+        request = dict(
+            meta=dict(ip='127.0.0.1', port=19234),
+            parameters=dict(url='/?404 NOT FOUND')
+        )
         environ, handler = Environ(request), WSGIHandler()
-        bson_binary = handler.call_application(no_json_response_application,
-                                               environ.get_dict())
-        self.assertEqual(dict(status=dict(reason='NOT FOUND', code='404'),
-                              no_json_response='Page Not Found'),
-                         bson.loads(bson_binary)
-                         )
+        bson_binary = handler.call_application(
+            no_json_response_application,
+            environ.get_dict()
+        )
+        self.assertEqual(
+            dict(
+                status=dict(reason='NOT FOUND', code='404'),
+                no_json_response='Page Not Found'
+            ),
+            bson.loads(bson_binary)
+        )
