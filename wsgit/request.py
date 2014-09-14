e39859d4735cd6ed9f573b602fb115fb4dd388b4
acuros <acuros@kaist.ac.kr>
2014-09-14 22:32:50 +0900

    AbstractRequest implemented

---
+++ wsgit/request.py
@@ -0,0 +1,40 @@
+class AbstractRequest(object):
+    TYPE_DETERMINER = None
+
+    def __init__(self, request_dict, meta=None):
+        if meta is None:
+            meta = dict()
+        if not isinstance(request_dict, dict):
+            raise TypeError(
+                'request_dict must be dict not %s' % type(request_dict)
+            )
+        if not isinstance(meta, dict):
+            raise TypeError('meta must be dict not %s' % type(meta))
+
+        self.request_dict = request_dict
+
+    @classmethod
+    def create(cls, request_dict):
+        if not isinstance(request_dict, dict):
+            return InvalidRequest(request_dict)
+        determiners = dict(
+            (RequestType.TYPE_DETERMINER, RequestType)
+            for RequestType in cls.__subclasses__()
+            if RequestType.TYPE_DETERMINER
+        )
+        request_class = determiners.get(request_dict.get('url', '\x00')[0])
+        if not request_class:
+            request_class = InvalidRequest
+        return request_class(request_dict)
+
+
+class WebRequest(AbstractRequest):
+    TYPE_DETERMINER = '/'
+
+
+class CommandRequest(AbstractRequest):
+    TYPE_DETERMINER = ':'
+
+
+class InvalidRequest(AbstractRequest):
+    pass
